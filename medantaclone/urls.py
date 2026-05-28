9aaf4f52f51ce59409124a2d9ec5cd8c90a55e9d
Drgauravmohan <dr.gauravmohan.cardiologist@gmail.com>
2026-05-29 01:26:47 +0530

    Add files via upload

--- medantaclone/urls.py
+++ medantaclone/urls.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
+from django.views.generic import TemplateView
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from django.views.generic import TemplateView
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('sitemap.xml', TemplateView.as_view(template_name='base/sitemap.xml', content_type='application/xml')),
     path('robots.txt', TemplateView.as_view(template_name='base/robots.txt', content_type='text/plain')),
-    path('admin/', admin.site.urls),
     path('', include('base.urls')),
 ]
 
